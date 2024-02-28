f45a44e28117e24c35894977fd0bb07c18300000
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-02-28 13:21:12 +0200

    Icons
    Added react-icons library and added icons where was needed.

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,15 +1,30 @@
+import { FaRegFileAlt } from "react-icons/fa"; //File icon for "content" button
+import { BiCustomize } from "react-icons/bi"; //icon for "customize" button
+import { FaTrash } from "react-icons/fa"; // icon for "clear" button
+import { GiGraduateCap } from "react-icons/gi"; //icon for "Education"
+import { FaBriefcase } from "react-icons/fa"; // icon for "experience"
+import { MdMail } from "react-icons/md"; //icon for "mail"
+import { FaPhoneAlt } from "react-icons/fa"; // icon for "phone"
+import { FaLocationDot } from "react-icons/fa6"; // icon for "location"
+
 export default function Layout() {
   return (
     <>
       <div className="app-div">
         <section className="customize-resume-container">
           <div className="sidebar">
-            <button>Content</button>
-            <button>Customize</button>
+            <button>
+              <FaRegFileAlt /> Content
+            </button>
+            <button>
+              <BiCustomize /> Customize
+            </button>
           </div>
           <div className="form-container">
             <div className="resume-controls">
-              <button>Clear Resume</button>
+              <button>
+                <FaTrash /> Clear Resume
+              </button>
               <button>Load Template</button>
             </div>
             <form className="personal-details">
@@ -34,10 +49,14 @@ export default function Layout() {
               <input type="text" placeholder="Bucuresti, Romania" />
             </form>
             <div className="education">
-              <button>Education</button>
+              <button>
+                <GiGraduateCap /> Education
+              </button>
             </div>
             <div className="experience">
-              <button>Experience</button>
+              <button>
+                <FaBriefcase /> Experience
+              </button>
             </div>
           </div>
         </section>
@@ -46,9 +65,15 @@ export default function Layout() {
             <div className="personal-info">
               <h1>Burciu Iulian Gabriel</h1>
               <div className="contact-info">
-                <span>Buiuga@gmail.com</span>
-                <span>077-012-3123</span>
-                <span>Pitesti, Romania</span>
+                <span>
+                  <MdMail /> Buiuga@gmail.com
+                </span>
+                <span>
+                  <FaPhoneAlt /> 077-012-3123
+                </span>
+                <span>
+                  <FaLocationDot /> Pitesti, Romania
+                </span>
               </div>
             </div>
             <div className="education-info">
