4d6bb03d89735dba847c120e2c75dd9d53126e26
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-02-28 18:55:09 +0200

    Added css
    Finished adding css and slightly changed html.

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,11 +1,12 @@
 import { FaRegFileAlt } from "react-icons/fa"; //File icon for "content" button
 import { BiCustomize } from "react-icons/bi"; //icon for "customize" button
 import { FaTrash } from "react-icons/fa"; // icon for "clear" button
-import { GiGraduateCap } from "react-icons/gi"; //icon for "Education"
+import { MdSchool } from "react-icons/md"; //icon for "Education"
 import { FaBriefcase } from "react-icons/fa"; // icon for "experience"
 import { MdMail } from "react-icons/md"; //icon for "mail"
 import { FaPhoneAlt } from "react-icons/fa"; // icon for "phone"
 import { FaLocationDot } from "react-icons/fa6"; // icon for "location"
+import { IoChevronDown } from "react-icons/io5"; // icon for dropdown menu
 
 export default function Layout() {
   return (
@@ -13,7 +14,7 @@ export default function Layout() {
       <div className="app-div">
         <section className="customize-resume-container">
           <div className="sidebar">
-            <button>
+            <button id="sidebar-active">
               <FaRegFileAlt /> Content
             </button>
             <button>
@@ -22,10 +23,10 @@ export default function Layout() {
           </div>
           <div className="form-container">
             <div className="resume-controls">
-              <button>
+              <button className="clear-resume-btn">
                 <FaTrash /> Clear Resume
               </button>
-              <button>Load Template</button>
+              <button className="load-template-btn">Load Template</button>
             </div>
             <form className="personal-details">
               <h3>Personal Details</h3>
@@ -38,49 +39,72 @@ export default function Layout() {
               <label>
                 Phone Number <span>recommended</span>
               </label>
-              <input
-                type="tel"
-                id="phone-number"
-                placeholder="Enter phone number"
-              />
+              <input type="tel" placeholder="Enter phone number" />
               <label>
                 Address <span>recommended</span>
               </label>
               <input type="text" placeholder="Bucuresti, Romania" />
             </form>
             <div className="education">
-              <button>
-                <GiGraduateCap /> Education
-              </button>
+              <span>
+                <MdSchool /> Education
+              </span>
+              <IoChevronDown />
             </div>
             <div className="experience">
-              <button>
+              <span>
                 <FaBriefcase /> Experience
-              </button>
+              </span>
+              <IoChevronDown />
             </div>
           </div>
         </section>
         <section className="resume-container">
-          <div className="resume">
-            <div className="personal-info">
-              <h1>Burciu Iulian Gabriel</h1>
-              <div className="contact-info">
-                <span>
-                  <MdMail /> Buiuga@gmail.com
-                </span>
-                <span>
-                  <FaPhoneAlt /> 077-012-3123
-                </span>
-                <span>
-                  <FaLocationDot /> Pitesti, Romania
-                </span>
+          <div className="personal-info">
+            <h1>Burciu Iulian Gabriel</h1>
+            <div className="contact-info">
+              <span>
+                <MdMail /> Buiuga@gmail.com
+              </span>
+              <span>
+                <FaPhoneAlt /> 077-012-3123
+              </span>
+              <span>
+                <FaLocationDot /> Pitesti, Romania
+              </span>
+            </div>
+          </div>
+          <div className="more-information">
+            <div className="education-section">
+              <h3>Education</h3>
+              <div className="education-info">
+                <div className="education-period">
+                  <p>08/2020 - present</p>
+                  <p>New York City, US</p>
+                </div>
+                <div className="school-and-degree">
+                  <p>London City University</p>
+                  <p>Bachelors in Economics</p>
+                </div>
               </div>
             </div>
-            <div className="education-info">
-              <p>Temporary...</p>
-            </div>
-            <div className="experience-info">
-              <p>Temporary...</p>
+            <div className="experience-section">
+              <h3>Professional Experience</h3>
+              <div className="experience-info">
+                <div className="working-period">
+                  <p>08/2020 - present</p>
+                  <p> New York City, US </p>
+                </div>
+                <div className="additional-informatio">
+                <p>Umbrella Inc</p>
+                <p>
+                  Designed and prototyped user interface patterns for various
+                  clients in various industries, ranging from self-service apps
+                  within the telecommunications-sector to mobile games for IOS
+                  and Android
+                </p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
