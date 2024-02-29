1202c77b9324372f742b90c176d8b718f52e4561
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-02-29 15:39:52 +0200

    Added input functionality
    Inputs now output value, also PersonalDetails from CV section outputs the input values.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,23 @@ import Experience from "./Components/Experience";
 import ResumePersonalInfo from "./Components/ResumePersonalInfo";
 import ResumeEducationInfo from "./Components/ResumeEducationInfo";
 import ResumeExperienceInfo from "./Components/ResumeExperienceInfo";
+import { useState } from "react";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+  });
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    // const name = e.target.name;
+    // const value = e.target.value;
+    setInputValue((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
   return (
     <>
       <div className="app-div">
@@ -15,13 +30,16 @@ export default function App() {
           <Sidebar />
           <div className="form-container">
             <ResumeControls />
-            <PersonalDetails />
+            <PersonalDetails
+              handleInputChange={handleInputChange}
+              inputValue={inputValue}
+            />
             <Education />
             <Experience />
           </div>
         </section>
         <section className="resume-container">
-          <ResumePersonalInfo />
+          <ResumePersonalInfo personalInfo={inputValue} />
           <div className="more-information">
             <ResumeEducationInfo />
             <ResumeExperienceInfo />
--- src/Components/PersonalDetails.jsx
+++ src/Components/PersonalDetails.jsx
@@ -1,21 +1,45 @@
-export default function PersonalDetails() {
+export default function PersonalDetails({ handleInputChange, inputValue }) {
   return (
     <form className="personal-details">
       <h3>Personal Details</h3>
       <label>Full name</label>
-      <input type="text" placeholder="Burciu Iulian Gabriel" />
+      <input
+        type="text"
+        placeholder="Burciu Iulian Gabriel"
+        name="name"
+        value={inputValue.name}
+        onChange={handleInputChange}
+      />
       <label>
         Email <span>recommended</span>
       </label>
-      <input type="email" placeholder="Buiuga@gmail.com" />
+      <input
+        type="email"
+        placeholder="Buiuga@gmail.com"
+        name="email"
+        value={inputValue.email}
+        onChange={handleInputChange}
+      />
       <label>
         Phone Number <span>recommended</span>
       </label>
-      <input type="tel" placeholder="Enter phone number" />
+      <input
+        type="tel"
+        placeholder="Enter phone number"
+        name="phoneNumber"
+        value={inputValue.phoneNumber}
+        onChange={handleInputChange}
+      />
       <label>
         Address <span>recommended</span>
       </label>
-      <input type="text" placeholder="Bucuresti, Romania" />
+      <input
+        type="text"
+        placeholder="Bucuresti, Romania"
+        name="address"
+        value={inputValue.address}
+        onChange={handleInputChange}
+      />
     </form>
   );
 }
