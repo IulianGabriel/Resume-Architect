b079e3eeebb04026e524b0a619ee0e5088bea630
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-02-29 15:55:18 +0200

    Clear and Load Template buttons.
    Added Control buttons / Clear and Load Template functionality, work as intended. +Util folder where i store template object.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Experience from "./Components/Experience";
 import ResumePersonalInfo from "./Components/ResumePersonalInfo";
 import ResumeEducationInfo from "./Components/ResumeEducationInfo";
 import ResumeExperienceInfo from "./Components/ResumeExperienceInfo";
+import templateResume from "./util/template";
 import { useState } from "react";
 
 export default function App() {
@@ -23,13 +24,35 @@ export default function App() {
       return { ...prev, [name]: value };
     });
   }
+
+  function handleClearButton() {
+    setInputValue({
+      name: "",
+      email: "",
+      phoneNumber: "",
+      address: "",
+    });
+  }
+
+  function handleTemplateButton() {
+    setInputValue({
+      name: templateResume.name,
+      email: templateResume.email,
+      phoneNumber: templateResume.phoneNumber,
+      address: templateResume.address,
+    });
+  }
+
   return (
     <>
       <div className="app-div">
         <section className="customize-resume-container">
           <Sidebar />
           <div className="form-container">
-            <ResumeControls />
+            <ResumeControls
+              handleClearButton={handleClearButton}
+              handleTemplateButton={handleTemplateButton}
+            />
             <PersonalDetails
               handleInputChange={handleInputChange}
               inputValue={inputValue}
---
+++ src/util/template.js
@@ -0,0 +1,8 @@
+const templateResume = {
+    name: "Samuel Parris",
+    email: "SamuelParris@gmail.com",
+    phoneNumber:"123-456-7890",
+    address:"London, UK",
+}
+
+export default templateResume;
